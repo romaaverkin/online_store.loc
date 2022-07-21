b28396e8a1118f32b4d5fb5a64fec5f2fc4fc60b
Roman <romaaverkin@gmail.com>
2022-07-21 11:15:08 +0700

    Подключил 3 шаблона

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('categories', function () {
+    return view('categories');
+});
+Route::get('product', function () {
+    return view('product');
 });
