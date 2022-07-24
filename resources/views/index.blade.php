9c6e5f6f72f2012dabf77572a51ba6b33e58c533
RomanAverkin <romaaverkin@gmail.com>
2022-07-24 16:16:12 +0700

    Сделал главный шаблон

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,41 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('master')
 
-    <title>Интернет Магазин</title>
+@section('title', 'Главная')
 
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/starter-template.css" rel="stylesheet">
-</head>
-<body>
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="/">Интернет Магазин</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li  class="active" ><a href="http://laravel-diplom-1.rdavydov.ru">Все товары</a></li>
-                <li ><a href="/categories">Категории</a>
-                </li>
-                <li ><a href="http://laravel-diplom-1.rdavydov.ru/basket">В корзину</a></li>
-                <li><a href="http://laravel-diplom-1.rdavydov.ru/reset">Сбросить проект в начальное состояние</a></li>
-            </ul>
-
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="http://laravel-diplom-1.rdavydov.ru/admin/home">Панель администратора</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
-
-<div class="container">
+@section('content')
     <div class="starter-template">
         <h1>Все товары</h1>
 
@@ -47,8 +14,9 @@
                         <h3>iPhone X 64GB</h3>
                         <p>71990 руб.</p>
                         <p>
-                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/1/add" class="btn btn-primary" role="button">В корзину</a>
-                            <a href="http://laravel-diplom-1.rdavydov.ru/mobiles/iphone_x_64" class="btn btn-default"
+                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/1/add" class="btn btn-primary"
+                               role="button">В корзину</a>
+                            <a href="/mobiles/iphone_x_64" class="btn btn-default"
                                role="button">Подробнее</a>
                         </p>
                     </div>
@@ -61,8 +29,9 @@
                         <h3>iPhone X 256GB</h3>
                         <p>89990 руб.</p>
                         <p>
-                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/2/add" class="btn btn-primary" role="button">В корзину</a>
-                            <a href="http://laravel-diplom-1.rdavydov.ru/mobiles/iphone_x_256" class="btn btn-default"
+                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/2/add" class="btn btn-primary"
+                               role="button">В корзину</a>
+                            <a href="/mobiles/iphone_x_256" class="btn btn-default"
                                role="button">Подробнее</a>
                         </p>
                     </div>
@@ -75,8 +44,9 @@
                         <h3>HTC One S</h3>
                         <p>12490 руб.</p>
                         <p>
-                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/3/add" class="btn btn-primary" role="button">В корзину</a>
-                            <a href="http://laravel-diplom-1.rdavydov.ru/mobiles/htc_one_s" class="btn btn-default"
+                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/3/add" class="btn btn-primary"
+                               role="button">В корзину</a>
+                            <a href="/mobiles/htc_one_s" class="btn btn-default"
                                role="button">Подробнее</a>
                         </p>
                     </div>
@@ -89,8 +59,9 @@
                         <h3>iPhone 5SE</h3>
                         <p>17221 руб.</p>
                         <p>
-                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/4/add" class="btn btn-primary" role="button">В корзину</a>
-                            <a href="http://laravel-diplom-1.rdavydov.ru/mobiles/iphone_5se" class="btn btn-default"
+                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/4/add" class="btn btn-primary"
+                               role="button">В корзину</a>
+                            <a href="/mobiles/iphone_5se" class="btn btn-default"
                                role="button">Подробнее</a>
                         </p>
                     </div>
@@ -103,8 +74,9 @@
                         <h3>Наушники Beats Audio</h3>
                         <p>20221 руб.</p>
                         <p>
-                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/5/add" class="btn btn-primary" role="button">В корзину</a>
-                            <a href="http://laravel-diplom-1.rdavydov.ru/portable/beats_audio" class="btn btn-default"
+                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/5/add" class="btn btn-primary"
+                               role="button">В корзину</a>
+                            <a href="/portable/beats_audio" class="btn btn-default"
                                role="button">Подробнее</a>
                         </p>
                     </div>
@@ -117,8 +89,9 @@
                         <h3>Камера GoPro</h3>
                         <p>12000 руб.</p>
                         <p>
-                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/6/add" class="btn btn-primary" role="button">В корзину</a>
-                            <a href="http://laravel-diplom-1.rdavydov.ru/portable/gopro" class="btn btn-default"
+                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/6/add" class="btn btn-primary"
+                               role="button">В корзину</a>
+                            <a href="/portable/gopro" class="btn btn-default"
                                role="button">Подробнее</a>
                         </p>
                     </div>
@@ -131,8 +104,10 @@
                         <h3>Камера Panasonic HC-V770</h3>
                         <p>27900 руб.</p>
                         <p>
-                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/7/add" class="btn btn-primary" role="button">В корзину</a>
-                            <a href="http://laravel-diplom-1.rdavydov.ru/portable/panasonic_hc-v770" class="btn btn-default"
+                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/7/add" class="btn btn-primary"
+                               role="button">В корзину</a>
+                            <a href="/portable/panasonic_hc-v770"
+                               class="btn btn-default"
                                role="button">Подробнее</a>
                         </p>
                     </div>
@@ -145,7 +120,8 @@
                         <h3>Кофемашина DeLongi</h3>
                         <p>25200 руб.</p>
                         <p>
-                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/8/add" class="btn btn-primary" role="button">В корзину</a>
+                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/8/add" class="btn btn-primary"
+                               role="button">В корзину</a>
                             <a href="http://laravel-diplom-1.rdavydov.ru/appliances/delongi" class="btn btn-default"
                                role="button">Подробнее</a>
                         </p>
@@ -159,7 +135,8 @@
                         <h3>Холодильник Haier</h3>
                         <p>40200 руб.</p>
                         <p>
-                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/9/add" class="btn btn-primary" role="button">В корзину</a>
+                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/9/add" class="btn btn-primary"
+                               role="button">В корзину</a>
                             <a href="http://laravel-diplom-1.rdavydov.ru/appliances/haier" class="btn btn-default"
                                role="button">Подробнее</a>
                         </p>
@@ -173,7 +150,8 @@
                         <h3>Блендер Moulinex</h3>
                         <p>4200 руб.</p>
                         <p>
-                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/10/add" class="btn btn-primary" role="button">В корзину</a>
+                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/10/add" class="btn btn-primary"
+                               role="button">В корзину</a>
                             <a href="http://laravel-diplom-1.rdavydov.ru/appliances/moulinex" class="btn btn-default"
                                role="button">Подробнее</a>
                         </p>
@@ -187,7 +165,8 @@
                         <h3>Мясорубка Bosch</h3>
                         <p>9200 руб.</p>
                         <p>
-                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/11/add" class="btn btn-primary" role="button">В корзину</a>
+                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/11/add" class="btn btn-primary"
+                               role="button">В корзину</a>
                             <a href="http://laravel-diplom-1.rdavydov.ru/appliances/bosch" class="btn btn-default"
                                role="button">Подробнее</a>
                         </p>
@@ -196,6 +175,4 @@
             </div>
         </div>
     </div>
-</div>
-</body>
-</html>
+@endsection
