1d7341dd81ab15cdf1c7cac2bdd8843216c54c22
Roman <romaaverkin@gmail.com>
2022-07-25 16:31:17 +0700

    В работе

--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -2,14 +2,12 @@
     <div class="thumbnail">
         <img src="/images/products/iphone_x.jpg" alt="iPhone X 64GB">
         <div class="caption">
-            <h3>iPhone X 64GB</h3>
-            <p>71990 руб.</p>
+            <h3>{{ $product->name }}</h3>
+            <p>{{ $product->price }} руб.</p>
             <p>
                 <a href="{{ route('basket') }}" class="btn btn-primary"
                    role="button">В корзину</a>
-                @isset($category)
-                    {{ $category->name }}
-                @endisset
+                {{ $product->getCategory()->name }}
                 <a href="/mobiles/iphone_x_64" class="btn btn-default"
                    role="button">Подробнее</a>
             </p>
