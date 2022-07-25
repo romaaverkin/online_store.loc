b79831cfc8f12eea2b6a30f00eaafa3d0d3c7443
Roman <romaaverkin@gmail.com>
2022-07-25 15:20:32 +0700

    Изменил пути в маршрутах шаблонов

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -18,20 +18,20 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
-            <a class="navbar-brand" href="/">Интернет Магазин</a>
+            <a class="navbar-brand" href="{{ route('index') }}">Интернет Магазин</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="http://laravel-diplom-1.rdavydov.ru">Все товары</a></li>
-                <li><a href="/categories">Категории</a>
+                <li class="active"><a href="{{ route('index') }}">Все товары</a></li>
+                <li><a href="{{ route('categories') }}">Категории</a>
                 </li>
-                <li><a href="/basket">В корзину</a></li>
-                <li><a href="http://laravel-diplom-1.rdavydov.ru/reset">Сбросить проект в начальное состояние</a></li>
+                <li><a href="{{ route('basket') }}">В корзину</a></li>
+                <li><a href="{{ route('index') }}">Сбросить проект в начальное состояние</a></li>
             </ul>
 
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="http://laravel-diplom-1.rdavydov.ru/admin/home">Панель администратора</a></li>
-            </ul>
+            {{--            <ul class="nav navbar-nav navbar-right">--}}
+            {{--                <li><a href="http://laravel-diplom-1.rdavydov.ru/admin/home">Панель администратора</a></li>--}}
+            {{--            </ul>--}}
         </div>
     </div>
 </nav>
